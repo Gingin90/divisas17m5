b94efee03b9bd7852792fc90eab0b37424f167cb
ginger cea <gingercea@gmail.com>
2023-07-23 17:33:39 -0400

    proyecto finalizado se separo con comillas dobles divisas

--- app/src/main/java/com/example/myapplication17m5/MainActivity.kt
+++ app/src/main/java/com/example/myapplication17m5/MainActivity.kt
@@ -8,7 +8,7 @@ import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var divisas = listOf<String>("USD,CLP,EUR")
+    private var divisas = listOf<String>("USD","CLP","EUR")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
